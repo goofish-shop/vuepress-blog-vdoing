7beabc5961c1db572b7bd6438dcabbdfb044f3ee
ahKevinXy <2502329620@qq.com>
2023-07-15 17:02:48 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+// config.js
+module.exports = {
+    theme: 'vdoing', // npm主题依赖包
+    // theme: require.resolve('../../vdoing'), // 使用本地主题包
+
+    title: 'goofish-shop vuepress-blog-vdoing',
+    // theme: "@vuepress/theme-blog",
+    base: "/vuepress-blog-vdoing/",
+    themeConfig: {
+        repo: 'https://github.com/goofish-shop/vuepress-blog-vdoing',
+
+    }
+}
