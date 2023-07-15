4aa66ef52c81e1d0eb6ac1b88f10090ff25f3515
ahKevinXy <2502329620@qq.com>
2023-07-15 17:09:30 +0800

    修改标题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     theme: 'vdoing', // npm主题依赖包
     // theme: require.resolve('../../vdoing'), // 使用本地主题包
 
-    title: 'goofish-shop vuepress-blog-vdoing',
+    title: 'vuepress-blog-vdoing',
     // theme: "@vuepress/theme-blog",
     base: "/vuepress-blog-vdoing/",
     themeConfig: {
